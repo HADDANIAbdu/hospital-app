26b1706b5dbde6791574aa81bf1787a8feef0dea
HADDANIAbdu <ab.haddani10@gmail.com>
2025-03-25 16:23:37 +0000

    Ajouter la pagination

--- src/main/java/org/example/app/HospitalAppApplication.java
+++ src/main/java/org/example/app/HospitalAppApplication.java
@@ -20,13 +20,13 @@ public class HospitalAppApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //using builder
-        Patient patient1 = Patient.builder().Fullname("abderazak Haddani").dateOfBirth(new Date()).Gender("Male")
-                .sick(true).score(12).build();
-        PatientRepository.save(patient1);
+        //Patient patient1 = Patient.builder().Fullname("Darmian Antony").dateOfBirth(new Date()).Gender("Male")
+        //        .sick(false).score(30).build();
+        //PatientRepository.save(patient1);
         //using constructor with arguments
-        Patient patient2 = new Patient(null, "amal ouassem", new Date(),
-                "female", false, 10);
-        PatientRepository.save(patient2);
+        //Patient patient2 = new Patient(null, "Yelmaz adyemi", new Date(),
+        //        "Male", true, 32);
+        //PatientRepository.save(patient2);
     }
 }
 
--- src/main/java/org/example/app/web/PatientController.java
+++ src/main/java/org/example/app/web/PatientController.java
@@ -3,9 +3,14 @@ package org.example.app.web;
 import lombok.AllArgsConstructor;
 import org.example.app.entities.Patient;
 import org.example.app.repositories.PatientRepository;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +20,18 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/patients")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+                        @RequestParam(value = "size", defaultValue = "4") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("nbPages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("patients", pagePatients.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("size", size);
         return "patients";
+    }
+    @PostMapping("/patients/changeSize")
+    public String changePageSize(@RequestParam("size") int size,
+            @RequestParam("currentPage") int currentPage) {
+        return "redirect:/patients?page=" + currentPage + "&size=" + size;
     }
 }
