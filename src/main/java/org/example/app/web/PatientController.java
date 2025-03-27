72c3328cac873f278416e83dad6f63524b9b4c9e
HADDANIAbdu <ab.haddani10@gmail.com>
2025-03-27 00:58:24 +0000

    Ajouter la suppression des patients

--- src/main/java/org/example/app/web/PatientController.java
+++ src/main/java/org/example/app/web/PatientController.java
@@ -8,9 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -20,7 +19,8 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/patients")
-    public String index(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+    public String index(Model model, RedirectAttributes red,
+                        @RequestParam(value = "page", defaultValue = "0") int page,
                         @RequestParam(value = "size", defaultValue = "4") int size,
                         @RequestParam(value = "keyword", defaultValue = "") String keyword) {
         Page<Patient> pagePatients = patientRepository.findByFullnameContains(keyword, PageRequest.of(page, size));
@@ -29,6 +29,19 @@ public class PatientController {
         model.addAttribute("currentPage", page);
         model.addAttribute("size", size);
         model.addAttribute("keyword", keyword);
+        red.addAttribute("success", null);
+        red.addAttribute("error", null);
         return "patients";
+    }
+
+    @PostMapping("/patients/delete/{id}")
+    public String delete(RedirectAttributes red, @PathVariable long id) {
+        Patient p = patientRepository.findById(id).orElse(null);
+        if(p == null) red.addFlashAttribute("error", "Error deleting Patient !");
+        else {
+            patientRepository.delete(p);
+            red.addFlashAttribute("success", "Patient deleted successfully !");
+        }
+        return "redirect:/patients";
     }
 }
